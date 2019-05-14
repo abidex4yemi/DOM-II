5e96ee632d3934737d5469fbbfbf6d6cdbf45115
abidex4yemi <abidex4yemi@gmail.com>
2019-05-14 16:14:20 +0100

    update sign up form js

--- js/index.js
+++ js/index.js
@@ -39,6 +39,12 @@ let ticking = false;
 const header = document.querySelector('.main-navigation');
 const navLinks = Array.from(document.querySelector('.nav').children);
 
+navLinks.forEach(link => {
+	link.addEventListener('click', evt => {
+		evt.preventDefault();
+	});
+});
+
 function doSomething(scroll_pos) {
 	// Do something with the scroll position
 	if (scroll_pos > 400) {
@@ -84,3 +90,42 @@ const signup = evt => {
 
 const pickContent = document.querySelector('.content-pick');
 pickContent.addEventListener('click', signup);
+
+// signup form
+const inputsArr = Array.from(document.querySelectorAll('.form-container input'));
+const errors = [];
+let remember = false;
+
+inputsArr.forEach(input => {
+	if (input.type === 'text' || input.type === 'password') {
+		input.addEventListener('focus', function() {
+			if (this.value.trim() < 3) {
+				this.nextElementSibling.style.display = 'block';
+				this.nextElementSibling.style.color = 'red';
+				this.nextElementSibling.style.fontSize = '16px';
+				this.style.outline = 0;
+				this.style.border = '1px solid red';
+				this.nextElementSibling.innerHTML = `${this.previousElementSibling.innerHTML} is required`;
+			}
+			console.log(errors);
+		});
+	}
+
+	if (input.type == 'checkbox') {
+		input.addEventListener('change', function() {
+			if (this.checked === true) {
+				remember = true;
+			}
+		});
+	}
+});
+
+//copy event
+const copyText = document.querySelector('.funbus-intro');
+
+copyText.addEventListener('copy', event => {
+	const selection = document.getSelection();
+	event.clipboardData.setData('text/plain', selection.toString().toUpperCase());
+	event.preventDefault();
+	copyText.style.color = 'maroon';
+});
